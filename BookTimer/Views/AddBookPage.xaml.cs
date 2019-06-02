b8bde768ed1d7e3e7fe7a7a6ce808e9f39e93261
Kubawoj98 <38566722+Kubawoj98@users.noreply.github.com>
2019-06-02 23:31:41 +0200

    AddBookPage
    Listview

--- BookTimer/Views/AddBookPage.xaml.cs
+++ BookTimer/Views/AddBookPage.xaml.cs
@@ -26,6 +26,7 @@ namespace BookTimer.Views
     /// <summary>
     /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
     /// </summary>
+    /// 
     public sealed partial class AddBookPage : Page
     {
         string path;
@@ -39,13 +40,11 @@ namespace BookTimer.Views
             ApplicationView.PreferredLaunchViewSize = new Size(360, 640);
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
             path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "bookdb.sqlite");
-            try
-            {
-                con = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+            
+            con = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
 
             con.CreateTable<Book>();
-            }
-            catch (Exception ex) { };
+           
         }
 
         private void YourLibraryButton_Click(object sender, RoutedEventArgs e)
@@ -70,16 +69,27 @@ namespace BookTimer.Views
 
         private void Show_Click(object sender, RoutedEventArgs e)
         {
-            var query = con.Table<Book>();
-            string result = String.Empty;
-            foreach(var item in query)
-            {
-                result = String.Format("{0} {1} {2}", item.Id, item.Author, item.Title);
-                Debug.WriteLine(result);
-            }
-               
+            
+
 
 
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            var query = con.Table<Book>();
+            string result = String.Empty;
+            foreach (var item in query)
+            {
+
+                result = String.Format("{0} {1} {2}", item.Id, item.Author, item.Title);
+                Debug.WriteLine(result);
+            }
+
+        }
+
+            
+        }
     }
-}
+
