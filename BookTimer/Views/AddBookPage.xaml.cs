b32ad6dfca6b294307f3995c129eb9c7606933e0
Alice Gajos <alicjawiktoriagajos@gmail.com>
2019-06-03 16:36:27 +0200

    Marge Conflict resolved

--- BookTimer/Views/AddBookPage.xaml.cs
+++ BookTimer/Views/AddBookPage.xaml.cs
@@ -1,23 +1,13 @@
-﻿using System;
+﻿using BookTimer.GoogleBooksApiCnct;
+using BookTimer.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using BookTimer.Views;
-using BookTimer.GoogleBooksApiCnct;
-using System.Xml.Linq;
-using BookTimer.Models;
-using System.Diagnostics;
 
 //Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=234238
 
