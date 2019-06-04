91bb3a9cdd85bee25dd131cdba8cfacf274f2661
Kubawoj98 <38566722+Kubawoj98@users.noreply.github.com>
2019-06-04 02:31:38 +0200

    db

--- BookTimer/DataBase.cs
+++ BookTimer/DataBase.cs
@@ -14,11 +14,27 @@ namespace BookTimer
     class Database
     {
         
-        public static SQLite.Net.SQLiteConnection con;
-        private SQLiteCommand sql_cmd;
-        public static string BAZA_DANYCH_KSIAZI_PLIK= "bookdb.sqlite";
-        public static string BAZA_DANYCH_LACZKA = "Data Source=" + BAZA_DANYCH_KSIAZI_PLIK + ";Version=3;New=False;Compress=True;";
-        
-     
+        //public static SQLite.Net.SQLiteConnection con;
+        //private SQLiteCommand sql_cmd;
+        ////public static string BAZA_DANYCH_KSIAZI_PLIK= "bookdb.sqlite";
+        //public static string BAZA_DANYCH_LACZKA = "Data Source=" + BAZA_DANYCH_KSIAZI_PLIK + ";Version=3;New=False;Compress=True;";
+
+
+        public string path;
+        public SQLite.Net.SQLiteConnection con;
+        public SQLite.Net.SQLiteConnection Polacz()
+        {
+            path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "bookdb.sqlite");
+
+            con = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+
+           // con.CreateTable<Book>();
+
+            return con;
+
+
+
+
+        }
     }
 }
